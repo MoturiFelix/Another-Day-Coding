1e6bef9d213bb683da7f623d87f89cd6060af498
MoturiFelix <moturifelix@gmail.com>
2024-04-28 15:15:27 +0300

    first commit

---
+++ random_module.py
@@ -0,0 +1,9 @@
+import random
+#this is a virtual toin course choosing randomly between Heads and Tails
+
+random_side = random.randint(0,1)
+
+if random_side == 1:
+  print ("Heads")
+else:
+  print("Tails")
